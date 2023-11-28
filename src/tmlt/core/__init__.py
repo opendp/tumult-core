a458d167b7746407d95be9f795d06d92226cfb19
Luke Hartman <luke.hartman@tmlt.io>
2023-11-28 19:29:54 +0000

    Add pyarrow-hotfix for 3.7, and use pyarrow ^14.0.1.
    See the discussion [here](https://gitlab.com/tumult-labs/tumult/-/issues/2985#note_1668665778).
    
    Link to Issue: tumult#2985
    
    ### For the Assignee:
    - [ ] Update the UNRELEASED sections of the changelogs for each project modified, 
            or explain in the MR description why no updates were necessary.
    
    ### For the Reviewer:
    - [ ] Confirm that an Issue is linked, if applicable.
    - [ ] Confirm that the assignee updated relevant changelogs or provided appropriate
        justification as to why no updates were necessary.
    
    This template lives [here](https://gitlab.com/tumult-labs/core/-/blob/dev/.gitlab/merge_request_templates/default_template.md)

--- src/tmlt/core/__init__.py
+++ src/tmlt/core/__init__.py
@@ -4,6 +4,13 @@
 # Copyright Tumult Labs 2023
 import warnings
 
+try:
+    # Addresses https://nvd.nist.gov/vuln/detail/CVE-2023-47248 for Python 3.7
+    # Python 3.8+ resolve this by using PyArrow >=14.0.1, so it may not be available
+    import pyarrow_hotfix
+except ImportError:
+    pass
+
 from tmlt.core.utils.configuration import check_java11
 
 warnings.filterwarnings(action="ignore", category=UserWarning, message=".*open_stream")
