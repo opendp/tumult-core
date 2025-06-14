62a5fc81650bb280c70452be31ed60901fc6e026
Tom Magerlein <dev@tom.magerlein.com>
2025-06-14 11:31:05 -0400

    Switch to uv for package management

--- src/tmlt/core/__init__.py
+++ src/tmlt/core/__init__.py
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Tumult Labs 2025
 
-__version__ = "0.0.0"
-__version_tuple__ = (0, 0, 0)
-
 import warnings
 
 import pandas as pd
 import setuptools  # TODO(#3258): This import provides a workaround for a bug in PySpark
 import typeguard
+
+# This version file is populated during build -- do not commit it.
+from ._version import __version__
 
 # By default, typeguard only checks the first element lists, but we want to
 # check the type of every list item.
@@ -19,13 +19,6 @@ typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.A
 pd.DataFrame.iteritems = (
     pd.DataFrame.items
 )  # https://github.com/YosefLab/Compass/issues/92#issuecomment-1679190560
-
-try:
-    # Addresses https://nvd.nist.gov/vuln/detail/CVE-2023-47248 for Python 3.7
-    # Python 3.8+ resolve this by using PyArrow >=14.0.1, so it may not be available
-    import pyarrow_hotfix
-except ImportError:
-    pass
 
 warnings.filterwarnings(action="ignore", category=UserWarning, message=".*open_stream")
 warnings.filterwarnings(
