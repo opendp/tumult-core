cd4e20f85f735dc1c980fe77eae6a032706619cb
dasm <maegereg@gmail.com>
2025-08-08 10:16:36 -0700

    Add a merge queue with lint, test, and docs jobs (#20)
    * Add merge queue pipeline with basic checks and tests.
    
    * Temporarily disable checking docs.tmlt.dev links.
    
    * Remove poetry specific option.
    
    * Remove test trigger.

--- doc/conf.py
+++ doc/conf.py
@@ -174,7 +174,11 @@ intersphinx_mapping = {
 }
 
 # The ACM website seems to have some sort of protection that foils the linkchecker.
-linkcheck_ignore = [r"https://doi.org/10.1145/2382196.2382264"]
+linkcheck_ignore = [
+    r"https://doi.org/10.1145/2382196.2382264",
+    # TODO: Re-enable when we got the docs site moved back to docs.tmlt.dev.
+    r"https://docs.tmlt.dev",
+]
 
 
 def skip_members(app, what, name, obj, skip, options):
